b66643934cbfd04e8b154c45383b15cb51105a34
taibaik <101733400+taibaik@users.noreply.github.com>
2022-03-16 23:18:47 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+hot_outside = False
+cold_outside = False
+
+if hot_outside:
+    print(f"It's hot outside! Let's wear less clothings...")
+elif cold_outside:
+    print(f"It's cold outside! Let's wear our coats first...")
+else:
+    print(f"It's an unusual weather today...")
+    
